2b89e6c5d4a1532d351ca1b89afa1a91150e506b
Amisha54 <amishakumari2705@gmail.com>
2023-09-26 20:41:03 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let hr = document.getElementById('hour');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
-let monthDisplay = document.getElementById('month');
-let dayDisplay = document.getElementById('day');
-let yearDisplay = document.getElementById('year');
 
 
 function displayTime(){
@@ -13,9 +10,7 @@ function displayTime(){
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
-    let month = date.toLocaleString('default', { month: 'long' });
-    let day = date.getDate();
-    let year = date.getFullYear();
+
 
     let hRotation = 30*hh + mm/2;
     let mRotation = 6*mm;
@@ -25,10 +20,6 @@ function displayTime(){
     min.style.transform = `rotate(${mRotation}deg)`;
     sec.style.transform = `rotate(${sRotation}deg)`;
 
-     // Update date display
-    monthDisplay.textContent = month;
-    dayDisplay.textContent = day;
-    yearDisplay.textContent = year;
 }
 
 setInterval(displayTime, 1000);
